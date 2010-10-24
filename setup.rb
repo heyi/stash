e990bf34f4a2f95e70fef36462f9a1e334ddcfdb
h3rald <h3rald@h3rald.com>
2010-10-24 20:29:40 +0200

    Updated vim files, added gitignore, added setup

---
+++ setup.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'highline/import'
+require 'pathname'
+
+@stash = Pathname.new Dir.pwd
+@home = Pathname.new ENV['HOME']
+@symlinks = ['.vim', '.vimrc', '.git', '.gitconfig', '.inputrc', '.gitignore']
+
+puts @stash
+
+unless (['setup.rb', '.vimrc', '.inputrc'] - @stash.children.map{|c| c.basename.to_s}).empty? && @stash != @home then
+	raise RuntimeError, "Not in stash" 
+end
+
+def overwrite_file(file)
+	agree("=> Overwrite '#{@home}/#{file}'? ")
+end
+
+def symlink_file(stash_file)
+	file = stash_file.basename
+	home_file = @home + file
+	if home_file.exist? || home_file.symlink? then
+		if overwrite_file(file) then
+			home_file.directory? ? home_file.rmtree : home_file.unlink 
+		else
+			return
+		end
+	end
+	home_file.make_symlink stash_file
+	puts "   #{home_file} -> #{stash_file}"
+end
+
+puts "Symlinking stash files..."
+@symlinks.each do |s|
+	symlink_file(@stash + s)
+end
+puts "Done."
+
+
